42707fd75695da89a40468d5f5dee520f54ca4f3
Dastah <155549125+Dastah@users.noreply.github.com>
2024-01-03 14:23:28 +0100

    Update logic.js

--- logic.js
+++ logic.js
@@ -55,80 +55,80 @@ function goToTop(){
 
 function lds() {
     TITLE.innerHTML = "Lágrimas de Sangre"
-    IMAGES.innerHTML = "<img class='img2' src='img/LDSPIRAL.jpg'><img class='img2' src='img/LDSPIRAL2.jpg'><img class='img2' src='img/LDSPIRAL3.jpg'><img class='img2' src='img/DE TRANQUIS2.jpg'><img class='img2' src='img/DE TRANQUIS3.jpg'><img class='img2' src='img/DE TRANQUIS4.jpg'><div class='goToTop' onclick='goToTop()'>▲</div>";
+    IMAGES.innerHTML = "<img class='img2' src='LDSPIRAL.jpg'><img class='img2' src='LDSPIRAL2.jpg'><img class='img2' src='LDSPIRAL3.jpg'><img class='img2' src='DE TRANQUIS2.jpg'><img class='img2' src='DE TRANQUIS3.jpg'><img class='img2' src='DE TRANQUIS4.jpg'><div class='goToTop' onclick='goToTop()'>▲</div>";
 }
 
 function microbio() {
     TITLE.innerHTML = "Microbio"
-    IMAGES.innerHTML = "<img class='img2' src='img/MICROBIO.jpg'><img class='img2' src='img/MICROBIO2.jpg'><img class='img2' src='img/MICROBIO3.jpg'><div class='goToTop' onclick='goToTop()'>▲</div>";
+    IMAGES.innerHTML = "<img class='img2' src='MICROBIO.jpg'><img class='img2' src='MICROBIO2.jpg'><img class='img2' src='MICROBIO3.jpg'><div class='goToTop' onclick='goToTop()'>▲</div>";
 }
 
 function awakate() {
     TITLE.innerHTML = "Awakate La Salsa"
-    IMAGES.innerHTML = "<img class='img2' src='img/AWAKATE LA SALSA.jpg'><img class='img2' src='img/AWAKATE LA SALSA2.jpg'><div class='goToTop' onclick='goToTop()'>▲</div>";
+    IMAGES.innerHTML = "<img class='img2' src='AWAKATE LA SALSA.jpg'><img class='img2' src='AWAKATE LA SALSA2.jpg'><div class='goToTop' onclick='goToTop()'>▲</div>";
 }
 
 function mama() {
     TITLE.innerHTML = "Mama Dousha"
-    IMAGES.innerHTML = "<img class='img2' src='img/MAMA DOUSHA.jpg'><img class='img2' src='img/MAMA DOUSHA2.jpg'><div class='goToTop' onclick='goToTop()'>▲</div>";
+    IMAGES.innerHTML = "<img class='img2' src='MAMA DOUSHA.jpg'><img class='img2' src='MAMA DOUSHA2.jpg'><div class='goToTop' onclick='goToTop()'>▲</div>";
 }
 
 function across() {
     TITLE.innerHTML = "Acros Massacre"
-    IMAGES.innerHTML = "<img class='img2' src='img/ACROS MASSACRE.jpg'><img class='img2' src='img/ACROS MASSACRE2.jpg'><img class='img2' src='img/ACROS MASSACRE3.jpg'><div class='goToTop' onclick='goToTop()'>▲</div>";
+    IMAGES.innerHTML = "<img class='img2' src='ACROS MASSACRE.jpg'><img class='img2' src='ACROS MASSACRE2.jpg'><img class='img2' src='ACROS MASSACRE3.jpg'><div class='goToTop' onclick='goToTop()'>▲</div>";
 }
 
 function anser() {
     TITLE.innerHTML = "Anser Exilio"
-    IMAGES.innerHTML = "<img class='img2' src='img/ANSER EXILIO.jpg'><img class='img2' src='img/ANSER EXILIO2.jpg'><img class='img2' src='img/ANSER EXILIO3.jpg'><div class='goToTop' onclick='goToTop()'>▲</div>";
+    IMAGES.innerHTML = "<img class='img2' src='ANSER EXILIO.jpg'><img class='img2' src='ANSER EXILIO2.jpg'><img class='img2' src='ANSER EXILIO3.jpg'><div class='goToTop' onclick='goToTop()'>▲</div>";
 }
 
 function lpdw() {
     TITLE.innerHTML = "LPDW"
-    IMAGES.innerHTML = "<img class='img2' src='img/LPDW.jpg'><img class='img2' src='img/LPDW2.jpg'><img class='img2' src='img/LPDW3.jpg'><div class='goToTop' onclick='goToTop()'>▲</div>";
+    IMAGES.innerHTML = "<img class='img2' src='LPDW.jpg'><img class='img2' src='LPDW2.jpg'><img class='img2' src='LPDW3.jpg'><div class='goToTop' onclick='goToTop()'>▲</div>";
 }
 
 function montserap() {
     TITLE.innerHTML = "MONTSERRAP"
-    IMAGES.innerHTML = "<img class='img2' src='img/MONTSERRAP.jpg'><img class='img2' src='img/MONTSERRAP2.jpg'><div class='goToTop' onclick='goToTop()'>▲</div>";
+    IMAGES.innerHTML = "<img class='img2' src='MONTSERRAP.jpg'><img class='img2' src='MONTSERRAP2.jpg'><div class='goToTop' onclick='goToTop()'>▲</div>";
 }
 
 function rie() {
     TITLE.innerHTML = "Rie Abril"
-    IMAGES.innerHTML = "<img class='img2' src='img/RIE ABRIL.jpg'><img class='img2' src='img/RIE ABRIL2.jpg'><img class='img2' src='img/RIE ABRIL3.jpg'><div class='goToTop' onclick='goToTop()'>▲</div>";
+    IMAGES.innerHTML = "<img class='img2' src='RIE ABRIL.jpg'><img class='img2' src='RIE ABRIL2.jpg'><img class='img2' src='RIE ABRIL3.jpg'><div class='goToTop' onclick='goToTop()'>▲</div>";
 }
 
 function levico() {
     TITLE.innerHTML = "Levico Tonto Listo"
-    IMAGES.innerHTML = "<img class='img2' src='img/LEVICO TONTO LISTO.jpg'><img class='img2' src='img/LEVICO TONTO LISTO2.jpg'><div class='goToTop' onclick='goToTop()'>▲</div>";
+    IMAGES.innerHTML = "<img class='img2' src='LEVICO TONTO LISTO.jpg'><img class='img2' src='LEVICO TONTO LISTO2.jpg'><div class='goToTop' onclick='goToTop()'>▲</div>";
 }
 
 function exodus() {
     TITLE.innerHTML = "Exodus Culture"
-    IMAGES.innerHTML = "<img class='img2' src='img/EXODUS CULTURE.jpg'><img class='img2' src='img/EXODUS CULTURE2.jpg'><img class='img2' src='img/EXODUS CULTURE3.jpg'><div class='goToTop' onclick='goToTop()'>▲</div>";
+    IMAGES.innerHTML = "<img class='img2' src='EXODUS CULTURE.jpg'><img class='img2' src='EXODUS CULTURE2.jpg'><img class='img2' src='EXODUS CULTURE3.jpg'><div class='goToTop' onclick='goToTop()'>▲</div>";
 }
 
 function ciniko() {
     TITLE.innerHTML = "Ciniko La Rabia del Pueblo"
-    IMAGES.innerHTML = "<img class='img2' src='img/CINIKO LA RABIA DEL PUEBLO.jpg'><img class='img2' src='img/CINIKO LA RABIA DEL PUEBLO2.jpg'><div class='goToTop' onclick='goToTop()'>▲</div>";
+    IMAGES.innerHTML = "<img class='img2' src='CINIKO LA RABIA DEL PUEBLO.jpg'><img class='img2' src='CINIKO LA RABIA DEL PUEBLO2.jpg'><div class='goToTop' onclick='goToTop()'>▲</div>";
 }
 
 function picapiedra() {
     TITLE.innerHTML = "Los Picapiedra"
-    IMAGES.innerHTML = "<img class='img2' src='img/LOS PICAPIEDRA.jpg'><img class='img2' src='img/LOS PICAPIEDRA2.jpg'><div class='goToTop' onclick='goToTop()'>▲</div>";
+    IMAGES.innerHTML = "<img class='img2' src='LOS PICAPIEDRA.jpg'><img class='img2' src='LOS PICAPIEDRA2.jpg'><div class='goToTop' onclick='goToTop()'>▲</div>";
 }
 
 function hardcore() {
     TITLE.innerHTML = "Hardcore"
-    IMAGES.innerHTML = "<img class='img2' src='img/HARDCORE.jpg'><img class='img2' src='img/HARDCORE2.jpg'><img class='img2' src='img/HARDCORE3.jpg'><div class='goToTop' onclick='goToTop()'>▲</div>";
+    IMAGES.innerHTML = "<img class='img2' src='HARDCORE.jpg'><img class='img2' src='HARDCORE2.jpg'><img class='img2' src='HARDCORE3.jpg'><div class='goToTop' onclick='goToTop()'>▲</div>";
 }
 
 function autobiografia() {
     TITLE.innerHTML = "Ciniko Autobiografía de un Artista Fracasado"
-    IMAGES.innerHTML = "<img class='img2' src='img/AUTOBIOGRAFIA.jpg'><div class='goToTop' onclick='goToTop()'>▲</div>";
+    IMAGES.innerHTML = "<img class='img2' src='AUTOBIOGRAFIA.jpg'><div class='goToTop' onclick='goToTop()'>▲</div>";
 }
 
 function jazzwoman() {
     TITLE.innerHTML = "Jazzwoman Cuervos"
-    IMAGES.innerHTML = "<img class='img2' src='img/JAZZWOMAN CUERVOS.jpg'><img class='img2' src='img/JAZZWOMAN CUERVOS2.jpg'><img class='img2' src='img/JAZZWOMAN CUERVOS3.jpg'><img class='img2' src='img/JAZZWOMAN CUERVOS4.jpg'><div class='goToTop' onclick='goToTop()'>▲</div>";
+    IMAGES.innerHTML = "<img class='img2' src='JAZZWOMAN CUERVOS.jpg'><img class='img2' src='JAZZWOMAN CUERVOS2.jpg'><img class='img2' src='JAZZWOMAN CUERVOS3.jpg'><img class='img2' src='JAZZWOMAN CUERVOS4.jpg'><div class='goToTop' onclick='goToTop()'>▲</div>";
 }
